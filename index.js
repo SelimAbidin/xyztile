bef4d99fd5bbc094201c5b41fe2ab6b3cab4231b
Yavuz Selim Abidin <ysabidin@gmail.com>
2018-01-29 22:11:24 +0300

    fix

--- index.js
+++ index.js
@@ -17,12 +17,12 @@ function lat2tile( lat, zoom )  {
 }
 
 function tile2long( x ,z ) {
-    return ( x / Math.pow( 2 ,z ) * 360 - 180 )
+    return (x/Math.pow(2,z)*360-180)
 }
    
 function tile2lat( y , z ) {
     let n = Math.PI - 2 * Math.PI * y / Math.pow(2,z)
-    return 180 / Math.PI * Math.atan(0.5 * ( Math.exp(n) - Math.exp(-n) ) )
+    return (180/Math.PI*Math.atan(0.5*(Math.exp(n)-Math.exp(-n))))
 }
 
 module.exports = {
